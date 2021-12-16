c8ea7d194dd7252013898868bd0e2fbad29db3d5
Jeremy Schulman <jeremy@apstra.com>
2021-12-15 19:15:28 -0500

    Jls dev dec15 (#5)

--- netcam_aiomeraki/meraki_dut.py
+++ netcam_aiomeraki/meraki_dut.py
@@ -17,9 +17,10 @@
 # -----------------------------------------------------------------------------
 
 import asyncio
-from typing import Optional, Dict, List, Union
+from typing import Optional, Dict, List, Union, TypeVar
 from os import environ
 from functools import partial, cached_property, singledispatchmethod, reduce
+from http import HTTPStatus
 
 # -----------------------------------------------------------------------------
 # Public Imports
@@ -29,7 +30,16 @@ from netcad.logger import get_logger
 from netcad.netcam.dut import AsyncDeviceUnderTest
 from netcad.netcam import CollectionTestResults
 from netcad.testing_services import TestCases
-from meraki.aio import AsyncDashboardAPI
+
+from meraki.aio import AsyncDashboardAPI, AsyncAPIError
+
+from tenacity import (
+    retry,
+    wait_exponential,
+    stop_after_attempt,
+    RetryCallState,
+    RetryError,
+)
 
 # -----------------------------------------------------------------------------
 # Exports
@@ -37,6 +47,7 @@ from meraki.aio import AsyncDashboardAPI
 
 __all__ = ["MerakiDeviceUnderTest", "TestCases", "CollectionTestResults"]
 
+AsyncAPIErrorLike = TypeVar("AsyncAPIErrorLike", AsyncAPIError, BaseException)
 
 # -----------------------------------------------------------------------------
 #
@@ -156,20 +167,101 @@ class MerakiDeviceUnderTest(AsyncDeviceUnderTest):
 
             return api_data[0]
 
-    async def ping_check(self, timeout=5):
-        ping_check = {"status": "none"}
+    async def ping_check(self, timeout=5) -> dict:
+        """
+        This function executes the Meraki Dashboard "check device connectivity"
+        test and returns the resulting API payload.  When the check passes then
+        the DUT attribute `meraki_device_reachable` is set to True, otherwise it
+        is set to False.
+
+        Parameters
+        ----------
+        timeout:
+            The number of secions to await the ping-check response before
+            returning a failure result.
+
+        Returns
+        -------
+        dict
+            The API payload of the ping check response, or a "sential" dict
+            value indicating a timeout.
+
+        Notes
+        -----
+        2021-dec-15/JLS: From asyncio concurrent testing, it appears that the
+        Meraki Dashboard can only support a maximum of 5 concurrent ping-check
+        tasks.  Ffffff. So now we need to add a backoff/retry mechanism to
+        handle this acase.
+        """
+
+        # initialize to timeout to handle case where the timeout condition
+        # occurs and we want to indicate that to the Caller.
+
+        ping_check = {"status": "timeout"}
+        log = get_logger()
+
+        def retry_on_429(retry_state: RetryCallState):
+            oc = retry_state.outcome
+            if not oc.failed:
+                return False
+
+            # if the exception is a 429, then we are going to backoff
+            # and try again.
+
+            rt_exc: AsyncAPIErrorLike = retry_state.outcome.exception()
+            if not hasattr(rt_exc, "status"):
+                return False
+
+            if rt_exc.status == HTTPStatus.TOO_MANY_REQUESTS:
+                log.info(
+                    f"DUT: {self.device.name}: Still working on Meraki ping request ..."
+                )
+                return True
+
+            # otherwise, we've tried hard enough and we should stop trying.
+            # this will result in a RetryError in the calling context.
+            return False
+
+        @retry(
+            retry=retry_on_429,
+            wait=wait_exponential(multiplier=1, min=4, max=10),
+            stop=stop_after_attempt(5),
+        )
+        async def _create_ping(_api):
+            return await api.devices.createDeviceLiveToolsPingDevice(serial=self.serial)
+
+        @retry(
+            retry=retry_on_429,
+            wait=wait_exponential(multiplier=1, min=4, max=10),
+            stop=stop_after_attempt(5),
+        )
+        async def _check_ping(_api, _job):
+            return await api.devices.getDeviceLiveToolsPingDevice(
+                serial=self.serial, id=_job["pingId"]
+            )
 
         async with self.meraki_api() as api:
-            ping_device = await api.devices.createDeviceLiveToolsPingDevice(
-                serial=self.serial
-            )
+            try:
+                ping_job = await _create_ping(api)
+
+            except (AsyncAPIError, RetryError):
+                log.error(
+                    f"DUT: {self.device.name}: Timeout starting Meraki ping check ... proceeding regardless"
+                )
+                self.meraki_device_reachable = True
+                return ping_check
 
             while timeout:
                 await asyncio.sleep(1)
 
-                ping_check = await api.devices.getDeviceLiveToolsPingDevice(
-                    serial=self.serial, id=ping_device["pingId"]
-                )
+                try:
+                    ping_check = await _check_ping(api, ping_job)
+                except (AsyncAPIError, RetryError):
+                    log.error(
+                        f"DUT: {self.device.name}: Timeout checking Meraki ping ... proceeding regardless"
+                    )
+                    self.meraki_device_reachable = True
+                    return ping_check
 
                 if ping_check["status"] == "complete":
                     break
@@ -200,7 +292,7 @@ class MerakiDeviceUnderTest(AsyncDeviceUnderTest):
 
         self.meraki_device = dev
 
-        log.info(f"DUT: {self.device.name}: Running connectivity ping check ...")
+        log.info(f"DUT: {self.device.name}: Running Meraki connectivity ping check ...")
 
         await self.ping_check()
         if not self.meraki_device_reachable:
